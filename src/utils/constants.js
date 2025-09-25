69a20dd2e4426991db2ec75f8d15ad1dde035835
Nadif Hasan Purno <57897673+nadif-hasan-purno@users.noreply.github.com>
2025-09-25 14:16:36 +0600

    ok lets see

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   import.meta.env.MODE === "development"
-    ? "https://ielts-prep.onrender.com"
-    : "/api";
+    ? "http://localhost:5000"
+    : "https://ielts-prep.onrender.com";
